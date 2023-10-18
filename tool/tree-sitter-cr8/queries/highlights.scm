3f0e88e0fbf70169b42603e1b6a7d48fb5cf8b00
cooperrunyan <c@cooperrunyan.com>
2023-10-17 21:52:29 -0700

    Add tree sitter parser for asm

---
+++ tool/tree-sitter-cr8/queries/highlights.scm
@@ -0,0 +1,48 @@
+
+(comment) @comment.line
+
+(arrow) @keyword
+(comma) @punctuation.delimiter
+(slash) @operator
+(asterisk) @operator
+(plus) @operator
+(minus) @operator
+(equals) @operator
+(paren_open) @punctuation.bracket
+(paren_close) @punctuation.bracket
+(meta_open) @punctuation.special
+(bracket_open) @punctuation.bracket
+(bracket_close) @punctuation.bracket
+(mustache_open) @punctuation.bracket
+(mustache_close) @punctuation.bracket
+(angle_open) @operator
+(angle_close) @operator
+(angle_angle_left) @operator
+(angle_angle_right) @operator
+(pipe) @operator
+(union) @keyword
+(ampersand) @operator
+(percent) @operator
+(dot) @punctuation.delimiter
+(colon) @punctuation.delimiter
+(semi) @punctuation.delimiter
+(coloncolon) @punctuation.delimiter
+(dollar) @constant.builtin
+(string) @string
+
+(label) @label
+
+(number) @constant
+(register) @variable.name
+
+(macro_variable) @variable.parameter
+(macro_variable_type) @type
+
+(meta 
+  (use
+    (coloncolon) @entity.namespace)
+(meta 
+  (use
+    (coloncolon) @entity.namespace))
+(meta 
+  (bracket_close) @punctuation.special)
